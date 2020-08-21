b97542e506ada531e93c97906eb70e3919dc7251
capos <camille-aime.possamai@wanadoo.fr>
2020-08-22 00:25:54 +0200

    idem commint précédent mais après ajout du fichier UserController modifié

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,6 +14,7 @@ class UserController extends AbstractController
      * affichage de la page profil d'un utilisateur
      * 
      * @Route("/user/{slug}", name="user_show")
+     * @IsGranted("ROLE_USER")
      */
     public function index(User $user)
     {
@@ -26,7 +27,8 @@ class UserController extends AbstractController
      * affiche la liste des membres
      * 
      * @Route( "/users_list", name="users_list")
-     *
+     * @IsGranted("ROLE_USER")
+     * 
      * @return void
      */
     public function users_list( UserRepository $repo) {
